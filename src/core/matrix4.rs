baa81eee25cf118d0d1dff5ab19e38dfcde4138e
Vinicius Agostini <vinicius.agostini19@gmail.com>
2023-01-04 18:09:15 -0300

    Implement minor, cofactor and determinant for Matrix4

--- src/core/matrix4.rs
+++ src/core/matrix4.rs
@@ -54,6 +54,30 @@ impl Matrix4 {
         m
     }
 
+    pub fn minor(&self, row: usize, col: usize) -> f64 {
+        self.submatrix(row, col).determinant()
+    }
+
+    pub fn cofactor(&self, row: usize, col: usize) -> f64 {
+        let minor = self.minor(row, col);
+
+        if (row + col) % 2 == 1 {
+            -minor
+        } else {
+            minor
+        }
+    }
+
+    pub fn determinant(&self) -> f64 {
+        let mut det = 0.0;
+
+        for col in 0..4 {
+            det += self[[0, col]] * self.cofactor(0, col);
+        }
+
+        det
+    }
+
     const IDENTITY: Matrix4 = Matrix4::from([
         [1.0, 0.0, 0.0, 0.0],
         [0.0, 1.0, 0.0, 0.0],
@@ -112,6 +136,8 @@ impl Mul for Matrix4 {
 
 #[cfg(test)]
 pub mod matrix_tests {
+    use float_cmp::assert_approx_eq;
+
     use super::*;
 
     #[test]
@@ -255,5 +281,46 @@ pub mod matrix_tests {
         let expected = Matrix3::from([[1.0, 3.0, 4.0], [5.0, 7.0, 8.0], [5.0, 3.0, 2.0]]);
 
         assert_eq!(m_sub, expected);
+    }
+
+    #[test]
+    fn minor_of_matrix4() {
+        let m = Matrix4::from([
+            [-2.0, -8.0, 3.0, 5.0],
+            [-3.0, 1.0, 7.0, 3.0],
+            [1.0, 2.0, -9.0, 6.0],
+            [-6.0, 7.0, 7.0, -9.0],
+        ]);
+
+        assert_approx_eq!(f64, m.minor(0, 0), 690.0);
+        assert_approx_eq!(f64, m.minor(1, 2), 431.0);
+        assert_approx_eq!(f64, m.minor(2, 3), 207.0);
+    }
+
+    #[test]
+    fn cofactor_of_matrix4() {
+        let m = Matrix4::from([
+            [-2.0, -8.0, 3.0, 5.0],
+            [-3.0, 1.0, 7.0, 3.0],
+            [1.0, 2.0, -9.0, 6.0],
+            [-6.0, 7.0, 7.0, -9.0],
+        ]);
+
+        assert_approx_eq!(f64, m.cofactor(0, 0), 690.0);
+        assert_approx_eq!(f64, m.cofactor(0, 1), 447.0);
+        assert_approx_eq!(f64, m.cofactor(0, 2), 210.0);
+        assert_approx_eq!(f64, m.cofactor(0, 3), 51.0);
+    }
+
+    #[test]
+    fn determinant_of_matrix4() {
+        let m = Matrix4::from([
+            [-2.0, -8.0, 3.0, 5.0],
+            [-3.0, 1.0, 7.0, 3.0],
+            [1.0, 2.0, -9.0, 6.0],
+            [-6.0, 7.0, 7.0, -9.0],
+        ]);
+
+        assert_approx_eq!(f64, m.determinant(), -4071.0);
     }
 }
