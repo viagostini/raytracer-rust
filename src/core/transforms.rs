7ae4518b7934b1b13826eb3d63006ea2019be4b2
Vinicius Agostini <vinicius.agostini19@gmail.com>
2023-01-06 14:51:35 -0300

    Implement scaling transformation

--- src/core/transforms.rs
+++ src/core/transforms.rs
@@ -11,11 +11,20 @@ impl Transforms {
             [0.0, 0.0, 0.0, 1.0],
         ])
     }
+
+    pub fn scaling(x: f64, y: f64, z: f64) -> Matrix4 {
+        Matrix4::from([
+            [x, 0.0, 0.0, 0.0],
+            [0.0, y, 0.0, 0.0],
+            [0.0, 0.0, z, 0.0],
+            [0.0, 0.0, 0.0, 1.0],
+        ])
+    }
 }
 
 #[cfg(test)]
 pub mod transforms_tests {
-    use crate::core::point::Point;
+    use crate::core::{point::Point, vector::Vector};
 
     use super::*;
 
@@ -38,6 +47,40 @@ pub mod transforms_tests {
 
         let actual = inv * p;
         let expected = Point::new(-8.0, 7.0, 3.0);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn multiplying_vector_by_scaling() {
+        let v = Vector::new(-4.0, 6.0, 8.0);
+        let t = Transforms::scaling(2.0, 3.0, 4.0);
+
+        let actual = t * v;
+        let expected = Vector::new(-8.0, 18.0, 32.0);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn multiplying_vector_by_inverse_scaling() {
+        let v = Vector::new(-4.0, 6.0, 8.0);
+        let t = Transforms::scaling(2.0, 3.0, 4.0);
+        let inv = t.inverse();
+
+        let actual = inv * v;
+        let expected = Vector::new(-2.0, 2.0, 2.0);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn multiplying_by_negative_scaling_is_reflection() {
+        let p = Point::new(2.0, 3.0, 4.0);
+        let t = Transforms::scaling(-1.0, 1.0, 1.0);
+
+        let actual = t * p;
+        let expected = Point::new(-2.0, 3.0, 4.0);
 
         assert_eq!(expected, actual);
     }
